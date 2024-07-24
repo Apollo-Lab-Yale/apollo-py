bdc4f4c1d2d04483c93bfd19e371e3125e611255
djrakita <djrakita@gmail.com>
2024-07-24 08:27:26 -0400

    generic matrix and vectors

--- apollo_py/apollo_py_numpy/apollo_py_numpy_linalg/vectors.py
+++ apollo_py/apollo_py_numpy/apollo_py_numpy_linalg/vectors.py
@@ -63,15 +63,13 @@ class V3(V):
         if self.array.shape != (3,):
             raise ValueError("V3 must be a 3-vector.")
 
-    def __getitem__(self, item):
-        return self.array[item]
-
-    def __setitem__(self, key, value):
-        self.array[key] = value
-
     def to_lie_alg_so3(self):
         from apollo_py.apollo_py_numpy.apollo_py_numpy_spatial.lie.so3 import LieAlgSO3
         return LieAlgSO3.from_euclidean_space_element(self.array)
+
+    def to_lie_alg_h1(self):
+        from apollo_py.apollo_py_numpy.apollo_py_numpy_spatial.lie.h1 import LieAlgH1
+        return LieAlgH1.from_euclidean_space_element(self.array)
 
     def __repr__(self) -> str:
         return f"V3(\n{np.array2string(self.array)}\n)"
@@ -85,12 +83,6 @@ class V6(V):
         super().__init__(array)
         if self.array.shape != (6,):
             raise ValueError("V6 must be a 6-vector.")
-
-    def __getitem__(self, item):
-        return self.array[item]
-
-    def __setitem__(self, key, value):
-        self.array[key] = value
 
     def __repr__(self) -> str:
         return f"V6(\n{np.array2string(self.array)}\n)"
