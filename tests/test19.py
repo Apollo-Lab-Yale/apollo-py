f3a59b2b61138791040509ec3b40930d41e75c4a
djrakita <djrakita@gmail.com>
2024-12-16 23:19:33 -0500

    wip

---
+++ tests/test19.py
@@ -0,0 +1,15 @@
+from apollo_toolbox_py.apollo_py.apollo_py_robotics.resources_directories import ResourcesRootDirectory
+from apollo_toolbox_py.apollo_py.extra_tensorly_backend import Device, DType
+from apollo_toolbox_py.apollo_py_tensorly.apollo_py_tensorly_robotics.urdf_tensorly_module import \
+    ApolloURDFTensorlyModule
+import tensorly as tl
+
+tl.set_backend('pytorch')
+
+r = ResourcesRootDirectory.new_from_default_apollo_robots_dir()
+s = r.get_subdirectory('ur5')
+c = s.to_chain()
+u = c.base_urdf_module
+
+uu = ApolloURDFTensorlyModule.from_urdf_module(u, Device.MPS, DType.Float32)
+print(uu)
